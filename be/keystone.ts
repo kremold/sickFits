9d341cac1698b827d6cb9e7b4ca940ec18000b0e
Kyle R <pacman611@hotmail.com>
2021-02-14 10:49:14 -0600

    Updating the seed-data script and enable the script in keystone file
    Updating the seed-data script and enable the script in keystone file

--- be/keystone.ts
+++ be/keystone.ts
@@ -8,6 +8,7 @@ import {
   withItemData,
   statelessSessions,
 } from '@keystone-next/keystone/session';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -39,7 +40,12 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
-      // TODO Add data seeding here
+      async onConnect(keystone) {
+        console.log('Connected to the database.');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       // TODO add schema items
