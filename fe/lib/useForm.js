6cb3d8693a9b996a212fe3abe8c7f1e24a923c0c
Kyle R <pacman611@hotmail.com>
2021-02-14 16:06:01 -0600

    Custom hook for form input

---
+++ fe/lib/useForm.js
@@ -0,0 +1,44 @@
+import { object } from 'prop-types';
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // create a state onject for inputs
+  const [inputs, setInputs] = useState(initial);
+
+  function handleChange(e) {
+    let { value, name, type } = e.target; // destructuring the state
+    if (type === 'number') {
+      // if input is of type number set it as number rather than a string that html forms return by default
+      value = parseInt(value);
+    }
+    if (type === 'file') {
+      value[0] = e.target.files;
+    }
+
+    setInputs({
+      // copy the existing state
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    // make array, map over it, set values to null or empty, then create a object again
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
+
+  // return the things we want to surface from this custom hook
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
--- fe/pages/sell.js
+++ fe/pages/sell.js
@@ -1,3 +1,9 @@
+import CreateProduct from '../components/CreateProduct';
+
 export default function Sell() {
-  return <p>Hey I'm the sell page</p>;
+  return (
+    <div>
+      <CreateProduct />
+    </div>
+  );
 }
