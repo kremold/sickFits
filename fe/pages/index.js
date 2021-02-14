c420a370a9ae7f758f43e859cb911d0be4c39ae1
Kyle R <pacman611@hotmail.com>
2021-02-14 13:43:01 -0600

    query products from db, style the products list

--- fe/pages/index.js
+++ fe/pages/index.js
@@ -1,7 +1,2 @@
-export default function IndexPage() {
-  return (
-    <div>
-      <p>Home!</p>
-    </div>
-  );
-}
+export { default } from './products'; // allows route to be the same as something else
+// homepage showing the same as the product page
