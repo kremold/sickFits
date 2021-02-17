65f25c3657fad790d7be106f9556ed1ed8413c3d
Kyle R <pacman611@hotmail.com>
2021-02-17 13:44:34 -0600

    password reset email

--- be/keystone.ts
+++ be/keystone.ts
@@ -9,6 +9,7 @@ import {
   statelessSessions,
 } from '@keystone-next/keystone/session';
 import { insertSeedData } from './seed-data';
+import { sendPasswordResetEmail } from './lib/mail';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -28,7 +29,7 @@ const { withAuth } = createAuth({
   },
   passwordResetLink: {
     async sendToken(args) {
-      console.log(args);
+      await sendPasswordResetEmail(args.token, args.identity);
     },
   },
 });
